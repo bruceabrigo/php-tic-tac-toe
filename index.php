2f5dd442ff7584705b7c98182b3529a8de4e4bdf
bruceabrigo <bruceabrigo@outlook.com>
2024-01-29 13:34:24 -0600

    Player O is randomly selected, x and o winners are working properly. Tie conditional is working properly

--- index.php
+++ index.php
@@ -1,66 +1,62 @@
 <?php
-    # define a variable to container a winner
-    # winner variable must contain a string variable
-    $winner = 'n';
-    # create a game-board array
-    $gameBoard = array ('', '', '', '', '', '', '', '', '',);
-    # if a variable isset (not null), check form data via HTTP POST method, check the $winner array via the submit button
-    if (isset ($_POST ['submitBtn'])) {
-        $gameBoard[0] = $_POST['box0'];
-        $gameBoard[1] = $_POST['box1'];
-        $gameBoard[2] = $_POST['box2'];
-        $gameBoard[3] = $_POST['box3'];
-        $gameBoard[4] = $_POST['box4'];
-        $gameBoard[5] = $_POST['box5'];
-        $gameBoard[6] = $_POST['box6'];
-        $gameBoard[7] = $_POST['box7'];
-        $gameBoard[8] = $_POST['box8'];
+$winner = 'n';
+$gameBoard = array('', '', '', '', '', '', '', '', '');
+
+if (isset($_POST['submitBtn'])) {
+    for ($i = 0; $i <= 8; $i++) {
+        $gameBoard[$i] = $_POST['gameBoard' . $i];
     }
-    # create a game winning conditional for player X
-    if (
-        ($gameBoard[0] == 'X' && $gameBoard[1] == 'X' && $gameBoard[2] == 'X') ||
-        ($gameBoard[3] == 'X' && $gameBoard[4] == 'X' && $gameBoard[5] == 'X') ||
-        ($gameBoard[6] == 'X' && $gameBoard[7] == 'X' && $gameBoard[8] == 'X') ||
-        ($gameBoard[0] == 'X' && $gameBoard[3] == 'X' && $gameBoard[6] == 'X') ||
-        ($gameBoard[2] == 'X' && $gameBoard[6] == 'X' && $gameBoard[8] == 'X') ||
-        ($gameBoard[2] == 'X' && $gameBoard[4] == 'X' && $gameBoard[6] == 'X') ||
-        ($gameBoard[1] == 'X' && $gameBoard[4] == 'X' && $gameBoard[7] == 'X') ||
-        ($gameBoard[2] == 'X' && $gameBoard[5] == 'X' && $gameBoard[8] == 'X') ||
-        ($gameBoard[0] == 'X' && $gameBoard[4] == 'X' && $gameBoard[8] == 'X')
-    ){
-        $winner = 'X';
-        print ("</br></br><strong>X WON!</strong></br>")
-    }
+}
+
+if (
+    ($gameBoard[0] == 'x' && $gameBoard[1] == 'x' && $gameBoard[2] == 'x') ||
+    ($gameBoard[3] == 'x' && $gameBoard[4] == 'x' && $gameBoard[5] == 'x') ||
+    ($gameBoard[6] == 'x' && $gameBoard[7] == 'x' && $gameBoard[8] == 'x') ||
+    ($gameBoard[0] == 'x' && $gameBoard[3] == 'x' && $gameBoard[6] == 'x') ||
+    ($gameBoard[2] == 'x' && $gameBoard[6] == 'x' && $gameBoard[8] == 'x') ||
+    ($gameBoard[2] == 'x' && $gameBoard[4] == 'x' && $gameBoard[6] == 'x') ||
+    ($gameBoard[1] == 'x' && $gameBoard[4] == 'x' && $gameBoard[7] == 'x') ||
+    ($gameBoard[2] == 'x' && $gameBoard[5] == 'x' && $gameBoard[8] == 'x') ||
+    ($gameBoard[0] == 'x' && $gameBoard[4] == 'x' && $gameBoard[8] == 'x')
+) {
+    $winner = 'x';
+    print("</br></br><strong>X WON!</strong></br>");
+} else {
     $blank = 0;
-    for ($i = 0; $i<=8; $i++) {
+    for ($i = 0; $i <= 8; $i++) {
         if ($gameBoard[$i] == '') {
             $blank = 1;
         }
     }
-    if ($blank == 1 $$ $winner == 'n') {
+
+    if ($blank == 0 && $winner == 'n') {
+        $winner = 't';
+        print("</br>TIED GAME!");
+    } elseif ($blank == 1 && $winner == 'n') {
         $i = rand() % 8;
-        while ($gameBoard[$i] != 'O'){
+        while ($gameBoard[$i] != '') {
             $i = rand() % 8;
         }
-        $gameBoard[$i] = 'O';
+        $gameBoard[$i] = 'o';
+
         if (
-            ($gameBoard[0] == 'O' && $gameBoard[1] == 'O' && $gameBoard[2] == 'O') ||
-            ($gameBoard[3] == 'O' && $gameBoard[4] == 'O' && $gameBoard[5] == 'O') ||
-            ($gameBoard[6] == 'O' && $gameBoard[7] == 'O' && $gameBoard[8] == 'O') ||
-            ($gameBoard[0] == 'O' && $gameBoard[3] == 'O' && $gameBoard[6] == 'O') ||
-            ($gameBoard[2] == 'O' && $gameBoard[6] == 'O' && $gameBoard[8] == 'O') ||
-            ($gameBoard[2] == 'O' && $gameBoard[4] == 'O' && $gameBoard[6] == 'O') ||
-            ($gameBoard[1] == 'O' && $gameBoard[4] == 'O' && $gameBoard[7] == 'O') ||
-            ($gameBoard[2] == 'O' && $gameBoard[5] == 'O' && $gameBoard[8] == 'O') ||
-            ($gameBoard[0] == 'O' && $gameBoard[4] == 'O' && $gameBoard[8] == 'O')
-        ){
-            $winner = 'O';
-            print ("</br></br><strong>O WON!</strong></br>")
-        } else if ($winner = 'n') {
-            print("</br>TIED GAME!");
+            ($gameBoard[0] == 'o' && $gameBoard[1] == 'o' && $gameBoard[2] == 'o') ||
+            ($gameBoard[3] == 'o' && $gameBoard[4] == 'o' && $gameBoard[5] == 'o') ||
+            ($gameBoard[6] == 'o' && $gameBoard[7] == 'o' && $gameBoard[8] == 'o') ||
+            ($gameBoard[0] == 'o' && $gameBoard[3] == 'o' && $gameBoard[6] == 'o') ||
+            ($gameBoard[2] == 'o' && $gameBoard[6] == 'o' && $gameBoard[8] == 'o') ||
+            ($gameBoard[2] == 'o' && $gameBoard[4] == 'o' && $gameBoard[6] == 'o') ||
+            ($gameBoard[1] == 'o' && $gameBoard[4] == 'o' && $gameBoard[7] == 'o') ||
+            ($gameBoard[2] == 'o' && $gameBoard[5] == 'o' && $gameBoard[8] == 'o') ||
+            ($gameBoard[0] == 'o' && $gameBoard[4] == 'o' && $gameBoard[8] == 'o')
+        ) {
+            $winner = 'o';
+            print("</br></br><strong>O WON!</strong></br>");
         }
     }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -69,7 +65,22 @@
     <link rel="stylesheet" href="styles.css">
     <title>TIC TAC TOE</title>
 </head>
-<body>
-    <form action="index.php"></form>
+<body align='center'>
+    <form name="tictactoe" action="index.php" method="post">
+        <?php
+        for ($i = 0; $i <= 8; $i++) {
+            printf('<input type="text" name="gameBoard%s" value="%s" id="box">', $i, $gameBoard[$i]);
+            if ($i == 2 || $i == 5 || $i == 8) {
+                print('<br>');
+            }
+        }
+
+        if ($winner == 'n') {
+            print('<input type="submit" name="submitBtn" value="PLAY" id="go">');
+        } else {
+            print('<input type="submit" name="newgameBtn" value="PLAY AGAIN" id="again" onclick="window.location.href=\'index.php\'">');
+        }
+        ?>
+    </form>
 </body>
 </html>
